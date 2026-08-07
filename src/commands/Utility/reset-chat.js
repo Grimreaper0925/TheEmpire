01ebf7edb0d6e733ee5b8be3e7c6a00018f2d55a
Grimreaper0925 <157101005+Grimreaper0925@users.noreply.github.com>
2026-08-07 04:02:11 -0700

    Refactor reset-chat command to use ES6 modules

--- src/commands/Utility/reset-chat.js
+++ src/commands/Utility/reset-chat.js
@@ -1,13 +1,26 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { successEmbed } from '../../utils/embeds.js';
+import { logger } from '../../utils/logger.js';
+import { replyUserError, ErrorTypes } from '../../utils/errorHandler.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-module.exports = {
+export default {
     data: new SlashCommandBuilder()
-        .setName('reset-chat')
-        .setDescription('Resets the channel by clearing all messages and starts a new period.')
+        .setName("reset-chat")
+        .setDescription("Resets the channel by clearing all messages and starts a new period.")
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
-    
-    async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true });
+    category: "Utility",
+
+    async execute(interaction, config, client) {
+        const deferSuccess = await InteractionHelper.safeDefer(interaction);
+        if (!deferSuccess) {
+            logger.warn(`Reset-chat interaction defer failed`, {
+                userId: interaction.user.id,
+                guildId: interaction.guildId,
+                commandName: 'reset-chat'
+            });
+            return;
+        }
 
         try {
             const channel = interaction.channel;
@@ -24,7 +37,10 @@ module.exports = {
 
         } catch (error) {
             console.error('Failed to reset channel:', error);
-            await interaction.editReply({ content: 'There was an error trying to reset this channel.' });
+            return await replyUserError(interaction, { 
+                type: ErrorTypes.UNKNOWN, 
+                message: 'There was an error trying to reset this channel.' 
+            });
         }
     },
 };
